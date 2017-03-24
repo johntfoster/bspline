db985ffbe6f6084f3ab08eb955d7f2aaecdb874c
Juha Jeronen <juha.jeronen@jyu.fi>
2017-03-24 14:07:38 +0200

    bspline.py: add higher-order differentiation (diff()) and collocation matrix generation (collmat())

--- bspline.py
+++ bspline.py
@@ -169,3 +169,125 @@ class Bspline():
             plt.plot(x,n)
 
         return plt.show()
+
+
+    def __diff_internal(self):
+        """Differentiate a B-spline once, and return the resulting coefficients and Bspline objects.
+
+This preserves the Bspline object nature of the data, enabling recursive implementation
+of higher-order differentiation (see `diff`).
+
+The value of the first derivative of `B` at a point `x` can be obtained as::
+
+    def diff1(B, x):
+        terms = B.__diff_internal()
+        return sum( ci*Bi(x) for ci,Bi in terms )
+
+Returns:
+    tuple of tuples, where each item is (coefficient, Bspline object).
+
+See:
+    `diff`: differentiation of any order >= 0
+"""
+        assert self.p > 0, "order of Bspline must be > 0"  # we already handle the other case in diff()
+
+        # https://www.cs.mtu.edu/~shene/COURSES/cs3621/NOTES/spline/B-spline/bspline-derv.html
+        #
+        t    = self.knot_vector
+        p    = self.p
+        Bi   = Bspline( t[:-1], p-1 )
+        Bip1 = Bspline( t[1:],  p-1 )
+
+        numer1 = +p
+        numer2 = -p
+        denom1 = t[p:-1]   - t[:-(p+1)]
+        denom2 = t[(p+1):] - t[1:-p]
+
+        with np.errstate(divide='ignore', invalid='ignore'):
+            ci   = np.where(denom1 != 0., (numer1 / denom1), 0.)
+            cip1 = np.where(denom2 != 0., (numer2 / denom2), 0.)
+
+        return ( (ci,Bi), (cip1,Bip1) )
+
+
+    def diff(self, order=1):
+        """Differentiate a B-spline `order` number of times.
+
+Parameters:
+    order:
+        int, >= 0
+
+Returns:
+    **lambda** `x`: ... that evaluates the `order`-th derivative of `B` at the point `x`.
+"""
+        order = int(order)
+        if order < 0:
+            raise ValueError("order must be >= 0, got %d" % (order))
+
+        if order == 0:
+            return self.__call__
+
+        if order > self.p:   # identically zero, but force the same output format as in the general case
+            dummy = self.__call__(0.)  # get number of basis functions and output dtype
+            nbasis = dummy.shape[0]
+            return lambda x: np.zeros( (nbasis,), dtype=dummy.dtype )  # accept but ignore input x
+
+        # At each differentiation, each term maps into two new terms.
+        # The number of terms in the result will be 2**order.
+        #
+        # This will cause an exponential explosion in the number of terms for high derivative orders,
+        # but for the first few orders (practical usage; >3 is rarely needed) the approach works.
+        #
+        terms = [ (1.,self) ]
+        for k in range(order):
+            tmp = []
+            for Ci,Bi in terms:
+                tmp.extend( (Ci*cn, Bn) for cn,Bn in Bi.__diff_internal() )  # NOTE: also propagate Ci
+            terms = tmp
+
+        # perform final summation at call time
+        return lambda x: sum( ci*Bi(x) for ci,Bi in terms )
+
+
+    def collmat(self, tau, deriv_order=0):
+        """Compute collocation matrix.
+
+Parameters:
+    tau:
+        Python list or rank-1 array, collocation sites
+    deriv_order:
+        int, >=0, order of derivative for which to compute the collocation matrix.
+        The default is 0, which means the function value itself.
+
+Returns:
+    A:
+        rank-2 array such that
+            A[i,j] = D**deriv_order B_j(tau[i])
+        where
+            D**k  = kth derivative (0 for function value itself)
+
+Example:
+    If the coefficients of a spline function are given in the vector c, then::
+
+        sum( A[i,:] * c[:] )
+
+    will give a rank-1 array of function values at the sites tau[i] that were supplied
+    to `collmat`.
+
+    Similarly for derivatives (if the supplied `deriv_order`> 0).
+
+"""
+        # get number of basis functions and output dtype
+        dummy = self.__call__(0.)
+        nbasis = dummy.shape[0]
+
+        tau = np.atleast_1d(tau)
+        if tau.ndim > 1:
+            raise ValueError("tau must be a list or a rank-1 array")
+
+        A = np.empty( (tau.shape[0], nbasis), dtype=dummy.dtype )
+        f = self.diff(order=deriv_order)
+        for i,taui in enumerate(tau):
+            A[i,:] = f(taui)
+
+        return np.squeeze(A)
