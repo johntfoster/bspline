9189798c331f07cf9c1def6c210f1b3aadc5c58d
Juha Jeronen <juha.jeronen@jyu.fi>
2017-08-08 13:06:38 +0300

    add module-level init script

---
+++ bspline/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+"""Compute B-spline basis functions via Cox - de Boor algorithm.
+
+Submodules:
+    bspline.bspline
+        OO interface (class Bspline)
+    bspline.splinelab
+        MATLAB-style interface and helper functions.
+
+By default, the Bspline class from bspline.bspline is imported into this namespace when this module is loaded.
+"""
+
+from __future__ import absolute_import
+
+# This is extracted automatically by the top-level setup.py.
+__version__ = '0.1.0'
+
+# add any imports here, if you wish to bring things into the library's top-level namespace when the library is imported.
+from .bspline import Bspline
+
